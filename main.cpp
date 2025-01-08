2e26a58f8e470e468b18fe424164b8ef90bc634b
fagner ferreira <fagnerrc12@gmail.com>
2025-01-08 20:51:36 -0300

    Enhance DFS implementation

--- main.cpp
+++ main.cpp
@@ -1,41 +1,126 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <tuple>
+#include <cstring>
+#include <iomanip>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+
 
 using namespace std;
 
-// Implement iterative depth-first search algorithm
-void dfs(int start, int n, vector<vector<int>>& graph) {
-    vector<bool> visited(n, false);
-    stack<int> stack;
-    stack.push(start);
-    visited[start] = true;
-    while (!stack.empty()) {
-        int node = stack.top();
-        stack.pop();
-        cout << node << " ";
-        for (int i = 0; i < graph[node].size(); i++) {
-            if (!visited[graph[node][i]]) {
-                stack.push(graph[node][i]);
-                visited[graph[node][i]] = true;
+const int space_size = 6;
+
+struct point {
+    int x, y;
+};
+
+const point dirs[] = {
+    {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+};
+
+bool operator==(const point& lhs, const point& rhs) {
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+void print_path(vector<point> path, int visited[space_size][space_size]) {
+    std::cout << "\x1b[H\x1b[J" << std::flush;
+    for (int i = 0; i < space_size; i++) {
+        for (int j = 0; j < space_size; j++) {
+            point p = { i, j };
+            auto it = std::find(path.begin(), path.end(), p);
+            if (it != path.end()) {
+                cout << "\033[32;1m" << setw(4) << visited[i][j] << "\033[0m" << setw(4) << " | ";
+            } else {
+                cout << setw(4) << visited[i][j] << setw(4) << " | ";
             }
         }
+        cout << "\n";
+    }
+}
+
+void print(point target, point next, point prev, int visited[space_size][space_size]) {
+    std::cout << "\x1b[H\x1b[J" << std::flush;
+    for (int i = 0; i < space_size; i++) {
+        for (int j = 0; j < space_size; j++) {
+            if (i == target.x && j == target.y) {
+                cout << setw(4) << "*" << setw(4) << " | ";
+                continue;
+            } else if (i == prev.x && j == prev.y) {
+                cout << "\033[1;31m" << setw(4) << "P" << "\033[0m" << setw(4) << " | ";
+            } else if (i == next.x && j == next.y) {
+                cout << "\033[1;36m" << setw(4) << "X" << "\033[0m" << setw(4) << " | ";
+            } else {
+                cout << setw(4) << visited[i][j] << setw(4) << " | ";
+            }
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void dfs(point start, point target) {
+    int visited[space_size][space_size];
+    std::memset(visited, -1, sizeof(visited));
+
+    stack<point> stack;
+    stack.push(start);
+    visited[start.x][start.y] = 0;
+    while (!stack.empty()) {
+        point node = stack.top();
+        stack.pop();
+        // cout << node.x << "," << node.y << "\n";
+        for (int i = 0; i < 4; i++) {
+            point next = { node.x + dirs[i].x, node.y + dirs[i].y };
+            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size) {
+                continue;
+            }
+            if (next.x == target.x && next.y == target.y) {
+                cout << "Found target\n" << visited[node.x][node.y] + 1 << "\n";
+                // return;
+            }
+            if (visited[next.x][next.y] == -1 ||
+                visited[node.x][node.y] + 1 < visited[next.x][next.y]) {
+                stack.push(next);
+
+                visited[next.x][next.y] = visited[node.x][node.y] + 1;
+                print(target, next, node, visited);
+            }
+        }
+    }
+
+    vector<point> path = { target };
+
+    while (target.x != start.x || target.y != start.y) {
+        cout << target.x << "," << target.y << "\n";
+        point last = target;
+        bool assigned = false;
+        for (int i = 0; i < 4; i++) {
+            point next = { last.x + dirs[i].x, last.y + dirs[i].y };
+            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size) {
+                continue;
+            }
+            if (!assigned) {
+                target = next;
+                assigned = true;
+            } else {
+                if (visited[next.x][next.y] < visited[target.x][target.y]) {
+                    target = next;
+                }
+            }
+        }
+        path.push_back(target);
+
+        print_path(path, visited);
     }
 }
 
 int main() {
     cout << "Hello, World!\n";
 
-    // call dfs
-    vector<vector<int>> graph = {
-        {1, 2},
-        {0, 3, 4},
-        {0, 5},
-        {1},
-        {1},
-        {2}
-    };
-    dfs(0, 6, graph);
+    dfs(point{ 0, 0 }, point{ 0, 6 });
 
     return 0;
 }
--- run.cmd
+++ run.cmd
@@ -1,2 +1,3 @@
-g++ -o main.exe main.cpp
+rd .\main.exe
+g++ -std=c++14 -Wall -Wextra -o main.exe main.cpp
 .\main.exe
