46abd3b7263a03519a05fbb0baaa698c72abcae9
fagner ferreira <fagnerrc12@gmail.com>
2025-01-12 09:58:17 -0300

    Refactor pathfinding functions for consistent formatting and improved readability

--- src/path_finding.cpp
+++ src/path_finding.cpp
@@ -7,23 +7,20 @@ string generate_log(
     int visited_qty,
     int generated_qty,
     block blocks[space_size][space_size],
-    string algorithm)
-{
+    string algorithm) {
     std::vector<point> path = {};
 
-    while (target.x != -1 && target.y != -1)
-    {
+    while (target.x != -1 && target.y != -1) {
         path.push_back(target);
         target = blocks[target.x][target.y].info.from;
     }
     stringstream ss;
     ss << algorithm << "," << visited_qty << "," << generated_qty << "," << path.size() << ",[";
-    for (auto p : path)
-    {
+    for (auto p : path) {
         ss << "[" << p.x << " " << p.y << "]";
     }
     ss << "], [" << start.x << " " << start.y << "], [" << target.x << " " << target.y
-       << "]";
+        << "]";
 
     return ss.str();
 }
@@ -32,8 +29,7 @@ string dfs(
     point start,
     point target,
     cost_fn cost,
-    block blocks[space_size][space_size], bool &shouldDraw)
-{
+    block blocks[space_size][space_size], bool& shouldDraw) {
     int visited_qty = 0;
     int generated_qty = 0;
 
@@ -41,24 +37,20 @@ string dfs(
     stack.push(start);
     blocks[start.x][start.y].info.cost = 0;
     blocks[start.x][start.y].info.step = 0;
-    blocks[start.x][start.y].info.from = {-1, -1};
-    while (!stack.empty())
-    {
+    blocks[start.x][start.y].info.from = { -1, -1 };
+    while (!stack.empty()) {
         point node = stack.top();
         stack.pop();
 
-        for (int i = 0; i < 4; i++)
-        {
-            point next = {node.x + dirs[i].x, node.y + dirs[i].y};
-            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size)
-            {
+        for (int i = 0; i < 4; i++) {
+            point next = { node.x + dirs[i].x, node.y + dirs[i].y };
+            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size) {
                 continue;
             }
             float costValue = cost(i, blocks[node.x][node.y].info.step + 1);
-            if (next.x == target.x && next.y == target.y)
-            {
+            if (next.x == target.x && next.y == target.y) {
                 cout << "Found target\n"
-                     << blocks[node.x][node.y].info.cost + 1 << "\n";
+                    << blocks[node.x][node.y].info.cost + 1 << "\n";
                 blocks[next.x][next.y].info.cost = blocks[node.x][node.y].info.cost + costValue;
                 blocks[next.x][next.y].info.step = blocks[node.x][node.y].info.step + 1;
                 blocks[next.x][next.y].info.from = node;
@@ -68,8 +60,7 @@ string dfs(
                 return generate_log(start, target, visited_qty, generated_qty, blocks, "dfs");
             }
 
-            if (blocks[next.x][next.y].info.cost == -1)
-            {
+            if (blocks[next.x][next.y].info.cost == -1) {
                 stack.push(next);
 
                 blocks[next.x][next.y].info.cost = blocks[node.x][node.y].info.cost + costValue;
@@ -85,22 +76,19 @@ string dfs(
     // calculate_path(start, target, blocks, shouldDraw);
 }
 
-string a_star(point start, point target, cost_fn cost, heuristic_fn heuristic, block nodes[space_size][space_size], bool &shouldDraw)
-{
+string a_star(point start, point target, cost_fn cost, heuristic_fn heuristic, block nodes[space_size][space_size], bool& shouldDraw) {
     vector<point> open;
     vector<point> closed;
 
     nodes[start.x][start.y].info.cost = 0;
     nodes[start.x][start.y].info.heuristic = heuristic(start, target);
     nodes[start.x][start.y].info.step = 0;
-    nodes[start.x][start.y].info.from = {-1, -1};
+    nodes[start.x][start.y].info.from = { -1, -1 };
 
     open.push_back(start);
 
-    while (!open.empty())
-    {
-        auto min = min_element(open.begin(), open.end(), [&](point a, point b)
-                               {
+    while (!open.empty()) {
+        auto min = min_element(open.begin(), open.end(), [&](point a, point b) {
             block nodeA = nodes[a.x][a.y];
             block nodeB = nodes[b.x][b.y];
             return nodeA.info.cost + heuristic(a, target) < nodeB.info.cost + heuristic(b, target); });
@@ -109,34 +97,27 @@ string a_star(point start, point target, cost_fn cost, heuristic_fn heuristic, b
 
         closed.push_back(current);
 
-        if (current.x == target.x && current.y == target.y)
-        {
+        if (current.x == target.x && current.y == target.y) {
             cout << "Found target\n";
             // calculate_path(start, target, nodes, shouldDraw);
             return generate_log(start, target, closed.size(), open.size(), nodes, "a_star");
         }
 
-        for (int i = 0; i < 4; i++)
-        {
-            point next = {current.x + dirs[i].x, current.y + dirs[i].y};
-            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size)
-            {
+        for (int i = 0; i < 4; i++) {
+            point next = { current.x + dirs[i].x, current.y + dirs[i].y };
+            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size) {
                 continue;
             }
 
-            if (find(closed.begin(), closed.end(), next) != closed.end())
-            {
+            if (find(closed.begin(), closed.end(), next) != closed.end()) {
                 continue;
             }
             float new_cost = nodes[current.x][current.y].info.cost + cost(i, nodes[current.x][current.y].info.step + 1);
 
             auto found = find(open.begin(), open.end(), next);
-            if (found == open.end())
-            {
+            if (found == open.end()) {
                 open.push_back(next);
-            }
-            else if (new_cost >= nodes[next.x][next.y].info.cost)
-            {
+            } else if (new_cost >= nodes[next.x][next.y].info.cost) {
                 continue;
             }
 
@@ -150,8 +131,7 @@ string a_star(point start, point target, cost_fn cost, heuristic_fn heuristic, b
     }
 }
 
-string bfs(point start, point target, cost_fn cost, block blocks[space_size][space_size], bool &shouldDraw)
-{
+string bfs(point start, point target, cost_fn cost, block blocks[space_size][space_size], bool& shouldDraw) {
     int visited_qty = 0;
     int generated_qty = 0;
 
@@ -160,26 +140,22 @@ string bfs(point start, point target, cost_fn cost, block blocks[space_size][spa
 
     blocks[start.x][start.y].info.cost = 0;
     blocks[start.x][start.y].info.step = 0;
-    blocks[start.x][start.y].info.from = {-1, -1};
+    blocks[start.x][start.y].info.from = { -1, -1 };
 
-    while (!queue.empty())
-    {
+    while (!queue.empty()) {
         point current = queue.front();
         queue.pop();
 
-        for (int i = 0; i < 4; i++)
-        {
-            point next = {current.x + dirs[i].x, current.y + dirs[i].y};
-            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size)
-            {
+        for (int i = 0; i < 4; i++) {
+            point next = { current.x + dirs[i].x, current.y + dirs[i].y };
+            if (next.x < 0 || next.x >= space_size || next.y < 0 || next.y >= space_size) {
                 continue;
             }
 
             float costValue = cost(i, blocks[current.x][current.y].info.step + 1);
-            if (next.x == target.x && next.y == target.y)
-            {
+            if (next.x == target.x && next.y == target.y) {
                 cout << "Found target\n"
-                     << blocks[current.x][current.y].info.cost + 1 << "\n";
+                    << blocks[current.x][current.y].info.cost + 1 << "\n";
                 blocks[next.x][next.y].info.cost = blocks[current.x][current.y].info.cost + costValue;
                 blocks[next.x][next.y].info.step = blocks[current.x][current.y].info.step + 1;
                 blocks[next.x][next.y].info.from = current;
@@ -188,8 +164,7 @@ string bfs(point start, point target, cost_fn cost, block blocks[space_size][spa
 
                 return generate_log(start, target, visited_qty, generated_qty, blocks, "bfs");
             }
-            if (blocks[next.x][next.y].info.cost == -1)
-            {
+            if (blocks[next.x][next.y].info.cost == -1) {
                 queue.push(next);
                 blocks[next.x][next.y].info.cost = blocks[current.x][current.y].info.cost + 1;
                 blocks[next.x][next.y].info.step = blocks[current.x][current.y].info.step + 1;
@@ -203,15 +178,15 @@ string bfs(point start, point target, cost_fn cost, block blocks[space_size][spa
 
 string dijkstra(point start, point target, cost_fn cost, block blocks[space_size][space_size], bool& shouldDraw) {
     priority_queue<pair<float, point>, vector<pair<float, point>>, greater<pair<float, point>>> pq;
-    blocks[start.x][start.y].info.cost = 0; 
-    pq.push({0, start}); 
+    blocks[start.x][start.y].info.cost = 0;
+    pq.push({ 0, start });
 
     while (!pq.empty()) {
         auto [current_cost, current] = pq.top();
         pq.pop();
 
         if (current == target) {
-            return generate_log(start, target, 0, 0, blocks, "dijkstra"); 
+            return generate_log(start, target, 0, 0, blocks, "dijkstra");
         }
 
         for (int i = 0; i < 4; i++) {
@@ -225,8 +200,8 @@ string dijkstra(point start, point target, cost_fn cost, block blocks[space_size
 
             if (new_cost < blocks[next.x][next.y].info.cost) {
                 blocks[next.x][next.y].info.cost = new_cost;
-                blocks[next.x][next.y].info.from = current; 
-                pq.push({new_cost, next});
+                blocks[next.x][next.y].info.from = current;
+                pq.push({ new_cost, next });
             }
         }
     }
